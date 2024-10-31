3250ab8d91a195c61d32d9d233d1e393df3389a3
Abed Alrahman Shabaan, SBC-SAS-ITD <abed.shabaan@swisscom.com>
2024-10-31 09:22:30 +0100

    Feature extraction

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from argparse import ArgumentParser, Namespace
-from typing import Literal
+from typing import Dict, Literal
 from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch
+from torch.functional import Tensor
 from torch.utils.data.dataloader import DataLoader
 from torchvision.datasets import ImageFolder
 from torchvision.utils import save_image
+from torchvision.transforms import v2
+from tqdm import tqdm
+from os import path
+import seaborn as sns
 
 class Args(Namespace):
     input_classes_path:str
@@ -12,6 +17,9 @@ class Args(Namespace):
     base_model:Literal['facenet','dummy']
     classifier:Literal['knn', 'linear', 'k-means', 'cos', 'logisitc', 'svm']
     cuda:bool
+    output_path:str
+    save_embeddings:bool
+    load_embeddings:bool
     # face_size:int
 
 
@@ -23,6 +31,9 @@ def parse_args() -> Namespace:
     parser.add_argument('--base-model', help='Name of the model used for facial feature extraction, defaults to facenet', choices=['facenet'], default='facenet')
     parser.add_argument('--classifier', help='Name of the classifier model, this will be trained on the provided input classes', default='knn', choices=['knn', 'linear', 'k-means', 'cos', 'logisitc', 'svm'])
     parser.add_argument('--cuda', help='Name of the classifier model, this will be trained on the provided input classes', action='store_true')
+    parser.add_argument('--output-path', help='Path to an output directory, the results will be saved here', default=None)
+    parser.add_argument('--save-embeddings', help='Save the feature embeddings to the output directory instead of continuing the classification', action='store_true')
+    parser.add_argument('--load-embeddings', help='Load the feature embeddings from the output directory instead of treating the raw images', action='store_true')
     # parser.add_argument('--face-size', help='Size of the detected face image in pixels. Image is square.', default=128, type=int)
     return parser.parse_args()
 
@@ -33,18 +44,44 @@ def main():
 
     device = torch.device('cuda:0' if args.cuda and torch.cuda.is_available() else 'cpu')
 
-    input_data = ImageFolder(args.input_classes_path,)
-    loader = DataLoader(dataset=input_data, num_workers=2)
+    if (not args.load_embeddings):
+        input_data = ImageFolder(args.input_classes_path)
+        transform = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float, scale=True), lambda x: x.unsqueeze(0)])
+        # input_data = ImageFolder(args.input_classes_path, transform=transform)
+        loader = DataLoader(dataset=input_data, collate_fn=lambda x: x[0])
 
-    detector = MTCNN(device=device).eval()
+        print('Creating models')
+        detector = MTCNN(device=device).eval()
+        extractor = InceptionResnetV1(pretrained='vggface2', classify=False, device=device).eval()
 
-    i=0
-    for data, label in input_data:
-        i+=1
-        face = detector(data)
-        save_image(face, fp=f'./output_{i}.png')
+        i=0
+        features = torch.zeros((len(input_data), 512))
+        labels = torch.Tensor(len(input_data))
+        for data, label in tqdm(loader, desc='Extracting features'):
+            face = detector(data)
+            face = transform(face)
+            features[i,:] = extractor(face)
+            labels[i] = label
+            i+=1
+            # with open('test.pt', 'wb') as f:
+            #     torch.save(features, f)
+        if args.save_embeddings:
+            with open(path.join(args.output_path, 'features.pt'), 'wb') as f:
+                torch.save({'data': features, 'labels': labels}, f)
+                exit(0)
+    else:
+        pass
 
-    extractor = InceptionResnetV1(pretrained='vggface2', classify=False, device=device).eval()
+
+
+LabeledDataset=Dict[Literal['data', 'labels'], torch.Tensor]
+def visualize_embeddings(dataset:LabeledDataset, n_dims:int=2, algorithm:Literal['tsne','pca']='pca') -> None:
+    features = dataset['data']
+    labels = dataset['labels']
+
+
+    
+
 
 if __name__ == '__main__':
     main()
