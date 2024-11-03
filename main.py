131b8e5d13aca6f9b960cca8d7c1b879eef7d2c0
Abed Alrahman Shabaan, SBC-SAS-ITD <abed.shabaan@swisscom.com>
2024-11-03 14:52:03 +0100

    Working cos and Linear classifiers

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ from argparse import ArgumentParser, Namespace
 from typing import Dict, Literal, TypedDict
 from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch
-from torch.functional import Tensor
+from torch import nn
 from torch.utils.data.dataloader import DataLoader
 from torchvision.datasets import ImageFolder
-from torchvision.utils import save_image
 from torchvision.transforms import v2
 from tqdm import tqdm
 from os import path
 import seaborn as sns
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 
 class Args(Namespace):
@@ -24,6 +23,8 @@ class Args(Namespace):
     output_path:str
     save_embeddings:bool
     load_embeddings:bool
+    viz:bool
+    n_dims:int
     # face_size:int
 
 class LabeledDataset(TypedDict):
@@ -33,23 +34,24 @@ class LabeledDataset(TypedDict):
 
 
 
-def parse_args() -> Namespace:
+def parse_args() -> Args:
     parser = ArgumentParser('Face Similarity Framework', description='A Python program to give similarity scores of a target face[class] with regards to multiple input classes. Each class is to contain multiple images of the same person', exit_on_error=True)
     parser.add_argument('--input-classes-path', help='Path to a folder containing the input classes. This is a directory, with 1 subdir per class bearing the classname and containing input images for that class', required=True)
     parser.add_argument('--target-class-path', help='Path to the target class. This is a folder containing the images of the target class', required=True)
     parser.add_argument('--base-model', help='Name of the model used for facial feature extraction, defaults to facenet', choices=['facenet'], default='facenet')
     parser.add_argument('--classifier', help='Name of the classifier model, this will be trained on the provided input classes', default='knn', choices=['knn', 'linear', 'k-means', 'cos', 'logisitc', 'svm'])
-    parser.add_argument('--cuda', help='Name of the classifier model, this will be trained on the provided input classes', action='store_true')
+    parser.add_argument('--cuda', help='Use GPU for torch-based models. Only effective if a GPU is available on the current machine, ignored otherwise', action='store_true')
     parser.add_argument('--output-path', help='Path to an output directory, the results will be saved here', default=None)
     parser.add_argument('--save-embeddings', help='Save the feature embeddings to the output directory instead of continuing the classification', action='store_true')
     parser.add_argument('--load-embeddings', help='Load the feature embeddings from the output directory instead of treating the raw images', action='store_true')
+    parser.add_argument('--viz', help='Visualize a 2 or 3 dimensional representaion of the images instead of classifying. Use with n-dims', action='store_true')
+    parser.add_argument('--n-dims', help='The number of dimensions for the visualization', choices=[2, 3], type=int)
     # parser.add_argument('--face-size', help='Size of the detected face image in pixels. Image is square.', default=128, type=int)
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    args : Args
 
     device = torch.device('cuda:0' if args.cuda and torch.cuda.is_available() else 'cpu')
 
@@ -66,16 +68,20 @@ def main():
         with open(path.join(args.output_path, 'input.pt'), 'rb') as f:
             result = torch.load(f)
 
-    pca = PCA(n_components=2).fit(result['data'])
 
     print(f'Handling target pictures')
     target_data = ImageFolder(args.target_class_path)
     loader = DataLoader(dataset=target_data, collate_fn=lambda x: x[0])
     target_result = extract_features(loader, device=device)
 
-    total_result = merge_datasets(result, target_result)
 
-    visualize_embeddings(dataset=total_result, reducer=pca)
+    if args.viz:
+        total_result = merge_datasets(result, target_result)
+        pca = PCA(n_components=args.n_dims).fit(result['data'])
+        visualize_embeddings(dataset=total_result, reducer=pca, n_dims=args.n_dims)
+
+    else:
+        print(classify_features(result, target_result, classifier=args.classifier))
 
 
 
@@ -130,14 +136,51 @@ def extract_features(loader:DataLoader, device:torch.device=torch.device('cpu'))
             #     torch.save(features, f)
     return {'data': features, 'labels':labels, 'key': idx_to_class}
 
+
 def merge_datasets(d1:LabeledDataset, d2:LabeledDataset) -> LabeledDataset:
     features = torch.cat((d1['data'], d2['data']), 0)
     label_offset = len(d1['key'])
     labels = torch.cat((d1['labels'], d2['labels'] + label_offset), 0)
-    keys = d1['key']
+    keys = d1['key'].copy()
     keys.update({k+label_offset:v for k,v in d2['key'].items()})
 
     return {'data': features, 'labels': labels, 'key':keys}
+
+
+def classify_features(train:LabeledDataset, target:LabeledDataset, classifier:str='knn') -> Dict[str, float]:
+    print(f'Using {classifier} classifier on input vectors of dimension {train["data"].size(1)}')
+
+    if classifier == 'knn':
+        model = KNeighborsClassifier().fit(train['data'].numpy(), train['labels'].numpy())
+        probas = model.predict_proba(target['data'].numpy()).mean(0)
+
+    elif classifier == 'cos':
+        pred = train['labels'][target['data'].matmul(train['data'].transpose(0,1)).argmax(1)]
+        probas = {}
+        for k in train['key']:
+            probas[k] = ((pred == k).sum()/pred.size(0)).item()
+
+        return probas
+    elif classifier == 'linear':
+        model = nn.Linear(512, len(train['key']))
+        optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+        ce_function = torch.nn.CrossEntropyLoss()
+        x, y = train['data'], train['labels'].to(int)
+        for epoch in tqdm(range(10)):
+            optimizer.zero_grad()
+            out = model(x)
+            ce_function(out, y)
+            optimizer.step()
+
+        with torch.no_grad():
+            out = model(target['data'])
+            print(out)
+            probas = nn.functional.softmax(out, dim=1).mean(0).numpy()
+
+    else:
+        raise ValueError(f'Unsupported model type {classifier}')
+
+    return {v:probas[k] for k,v in train['key'].items()}
 
 if __name__ == '__main__':
     main()
